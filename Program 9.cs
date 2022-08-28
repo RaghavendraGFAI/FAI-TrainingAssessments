2b5e78d8bd20a9eb9b4649cf5c5b52ea830d8bc9
RaghavendraGFAI <112172318+RaghavendraGFAI@users.noreply.github.com>
2022-08-29 03:53:55 +0530

    Add files via upload

---
+++ Program 9.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Program9
+{
+    class Reverse
+    {
+        public static string reverseByWords(string Input)
+        {
+            char[] charArray = Input.ToCharArray();
+            string word = String.Empty;
+            for (int i = charArray.Length - 1; i > -1; i--)
+            {
+                word += charArray[i];
+            }
+
+
+            return word;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string:");
+            string str = Console.ReadLine();
+            string rev = reverseByWords(str);
+            Console.WriteLine("Reversed String is");
+            Console.WriteLine(rev);
+
+        }
+    }
+}
