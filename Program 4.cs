8461791606b1a608e3d4f4c2e853f4766fd5a335
RaghavendraGFAI <112172318+RaghavendraGFAI@users.noreply.github.com>
2022-08-29 02:46:20 +0530

    Add files via upload

---
+++ Program 4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Program3
+{
+    class EvenandOddSum
+    {
+        static void Sum()
+        {
+            int[] array = new int[1000];
+            int evensum = 0;
+            int oddsum = 0;
+            Console.WriteLine("Enter the total Number of Elements");
+            int count = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Elements: ");
+            for (int i=0; i<count; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+                if (array[i] % 2 == 0)
+                {
+
+                    evensum += array[i];
+                }
+                else
+                {
+                    oddsum += array[i];
+                }
+            }
+            Console.WriteLine($"Sum Of Even Numbers in the list of elements are : {evensum}");
+            Console.WriteLine($"Sum Of Odd Numbers in the list of elements are {oddsum}");
+
+
+        }
+        static void Main(string[] args)
+        {
+            Sum();
+        }
+    }
+}
