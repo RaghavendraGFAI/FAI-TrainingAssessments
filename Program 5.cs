9b072aa1c0d09a08ac4ba3b7fb97095565b13f19
RaghavendraGFAI <112172318+RaghavendraGFAI@users.noreply.github.com>
2022-08-29 02:57:11 +0530

    Add files via upload

---
+++ Program 5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Program5
+{
+    class Interest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Principle Amount");
+            double Amount = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Rate of Interest");
+            double Rate = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter time period");
+            double Time = double.Parse(Console.ReadLine());
+            double SInterest=SimpleInterest(Amount,Rate,Time);
+            Console.WriteLine($"The Interest is {SInterest}");
+        }
+        static double SimpleInterest(double amt,double roi,double time)
+        {
+            double Interest = ((amt*roi*time)/100);
+            return Interest;
+        }
+    }
+}
